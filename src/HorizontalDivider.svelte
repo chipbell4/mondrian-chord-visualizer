84c8e5c4d23a2f8048ba5604bee6206977f03184
Chip Bell <chipbell4@gmail.com>
2023-12-03 13:21:55 -0500

    Support vertical dividers too in one component

--- src/HorizontalDivider.svelte
+++ src/HorizontalDivider.svelte
@@ -1,20 +1,27 @@
 <script>
 import { MONDRIAN_BLUE, MONDRIAN_RED } from "./colors";
+import { HORIZONTAL, VERTICAL } from "./divider_direction";
 
-export let leftColor = MONDRIAN_BLUE;
-export let rightColor = MONDRIAN_RED;
+export let firstColor = MONDRIAN_BLUE;
+export let secondColor = MONDRIAN_RED;
+export let direction = HORIZONTAL;
 
 let container;
 let divider;
 
-let leftFlex = 1;
-let rightFlex = 1;
+let firstFlex = 1;
+let secondFlex = 1;
 
 let dividerClicked = false;
-let yOffset = 0;
+let offset = 0;
 const markDividerClicked = (e) => {
     dividerClicked = true;
-    yOffset = e.clientY - divider.getBoundingClientRect().top;
+    if (direction === VERTICAL) {
+        offset = e.clientY - divider.getBoundingClientRect().top;
+    } else {
+        offset = e.clientX - divider.getBoundingClientRect().left;
+    }
+    
 }
 const markDividerUnclicked = () => dividerClicked = false;
 const moveDivider = (e) => {
@@ -22,60 +29,76 @@ const moveDivider = (e) => {
         return;
     }
 
-    const desiredDividerY = e.clientY - yOffset;
-
     const containerRect = container.getBoundingClientRect();
 
-    leftFlex = desiredDividerY;
-    rightFlex = containerRect.height - 20 - desiredDividerY;
+    if (direction === VERTICAL) {
+        const desiredDividerPosition = e.clientY - offset;
+        firstFlex = desiredDividerPosition;
+        secondFlex = containerRect.height - 20 - desiredDividerPosition;
+    } else {
+        const desiredDividerPosition = e.clientX - offset;
+        firstFlex = desiredDividerPosition;
+        secondFlex = containerRect.width - 20 - desiredDividerPosition;
+    }
 };
 
 </script>
 
-<style>
+<style lang="scss">
 .container {
     width: 100%;
     height: 100%;
     display: flex;
-    flex-direction: column;
+    
     user-select: none;
     -webkit-user-select: none;
 }
 
-.left, .right, .divider {
-    width: 100%;
+.container.VERTICAL {
+    flex-direction: column;
+
+    .left, .right, .divider {
+        width: 100%;
+    }
+
+    .divider {
+        max-height: 20px;
+        height: 20px;
+    }
 }
 
-.left {
-    background: blue;
-}
+.container.HORIZONTAL {
+    flex-direction: row;
 
-.right {
-    background: red;
+    .left, .right, .divider {
+        height: 100%;
+    }
+
+    .divider {
+        width: 20px;
+    }
 }
 
 .divider {
     background: #111;
-    max-height: 20px;
-    height: 20px;
-}
 
-.divider:active {
-    background: #222;
+    &:active {
+        background: #222;
+    }
 }
 </style>
 
-<div class="container" bind:this={container}
+<div class="container {direction}" bind:this={container}
     on:mouseup={markDividerUnclicked}
     on:mouseleave={markDividerUnclicked}
     on:mousemove={moveDivider}
     >
-    <div class="left" style="flex: {leftFlex}; background: {leftColor}"></div>
+    <div class="left" style="flex: {firstFlex}; background: {firstColor}"></div>
     <div class="divider"
         bind:this={divider}
         on:mousedown={markDividerClicked}
         role="button"
         tabindex="0"
         ></div>
-    <div class="right" style="flex: {rightFlex}; background: {rightColor}"></div>
+    <div class="right" style="flex: {secondFlex}; background: {secondColor}"></div>
 </div>  
