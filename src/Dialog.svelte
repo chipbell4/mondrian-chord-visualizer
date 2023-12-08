b9d0c72d3a32405713ec812ed4b1923b4f80b5cf
Chip Bell <chipbell4@gmail.com>
2023-12-08 18:55:04 -0500

    A better dialog

--- src/Dialog.svelte
+++ src/Dialog.svelte
@@ -1,4 +1,20 @@
-<dialog open>
+<script>
+    import { onMount } from "svelte";
+
+    let dialog;
+
+    onMount(() => {
+        dialog.showModal();
+    })
+</script>
+
+<style>
+    ::backdrop {
+        backdrop-filter: blur(6px);
+    }
+</style>
+
+<dialog bind:this={dialog}>
     <p>Hello world!</p>
     <form method="dialog">
         <button>OK</button>
