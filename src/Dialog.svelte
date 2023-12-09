9d27ec06695778cbcb40e4eeedef7aa47b993ba2
Chip Bell <chipbell4@gmail.com>
2023-12-08 19:04:21 -0500

    Describing what the heck is going on here

--- src/Dialog.svelte
+++ src/Dialog.svelte
@@ -9,13 +9,32 @@
 </script>
 
 <style>
+    dialog {
+        font-family: Arial, Helvetica, sans-serif;
+        max-width: 600px;
+    }
+
+    h1 {
+        text-align: center;
+    }
+
+    form {
+        text-align: center;
+    }
+
     ::backdrop {
         backdrop-filter: blur(6px);
     }
 </style>
 
 <dialog bind:this={dialog}>
-    <p>Hello world!</p>
+    <h1>Mondrian Chord-Visualizer</h1>
+    <p>
+        This experiment visualizes microtonal chords using a Piet Mondrian-style generated image.
+        On the left is a resizable image (click the black bars!).
+        As the image proportions change, the notes in the chord change to reflect the new area of each sub-rectangle.
+        On the right, we notate the actual chord being played.
+    </p>
     <form method="dialog">
         <button>OK</button>
     </form>
